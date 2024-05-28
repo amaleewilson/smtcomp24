0036b7f91bae560787d3cae5e52ea0c2f1ed50f6
Amalee Wilson <amalee@cs.stanford.edu>
2024-05-28 15:14:26 -0700

    updated to aws testing command

--- docker/cvc5-images/leader/run_solver.sh
+++ docker/cvc5-images/leader/run_solver.sh
@@ -4,14 +4,14 @@
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 export RDMAV_FORK_SAFE=1
 
-### LOCAL TESTING ###
-mpirun -n 8 --mca btl_tcp_if_include eth0 --allow-run-as-root --hostfile $1 --bind-to none \
-    python3.8 /competition/do_solving.py /competition/cvc5 $2 $1
-
-
-# ### AWS TESTING ###
-# mpirun -n 20 --mca btl_tcp_if_include eth0 --allow-run-as-root --hostfile $1 --bind-to none \
+# ### LOCAL TESTING ###
+# mpirun -n 8 --mca btl_tcp_if_include eth0 --allow-run-as-root --hostfile $1 --bind-to none \
 #     python3.8 /competition/do_solving.py /competition/cvc5 $2 $1
+
+
+### AWS TESTING ###
+mpirun -n 16 --mca btl_tcp_if_include eth0 --allow-run-as-root --hostfile $1 --bind-to none \
+    python3.8 /competition/do_solving.py /competition/cvc5 $2 $1
 
 # ### COMPETITION OFFICIAL ###
 # mpirun -n 400 --mca btl_tcp_if_include eth0 --allow-run-as-root --hostfile $1 --bind-to none \
